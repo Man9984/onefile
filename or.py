a7450be3076d06a391480b662e18d18ab2817eb2
Manjeet <manjeetyadav9984@gmail.com>
2021-10-03 07:17:04 +0530

    i dont know what is adding

--- or.py
+++ or.py
@@ -7,7 +7,7 @@ import os
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
 log_dir = "logs"
 os.makedirs(log_dir, exist_ok=True)
-logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str, filemode="a")
+logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.info, format=logging_str, filemode="a")
 
 
 def main(data, modelName, plotName, eta, epochs):
